71a810096919a237352f1b3103ab1bfde8f8df82
Anderson <andercamar@hotmail.com>
2021-09-07 11:40:17 -0300

    a

--- display.py
+++ display.py
@@ -26,8 +26,8 @@ def getWeather():
     json_resp = response.json()
     temp = json_resp['current']['temp']
     feels = json_resp['current']['feels_like']
-    weather = json_resp['current']['wheather']
-    print(json_resp['current']['wheather'])
+    weather = json_resp['current']['weather']
+    print(json_resp['current']['weather'])
     data = {
         "temp":temp,
         "feels":feels
