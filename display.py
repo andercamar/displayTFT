bf6c00f2bcef4ef88d1fa849180fbf0006b02b7f
Anderson <andercamar@hotmail.com>
2021-09-07 10:12:21 -0300

    test font

--- display.py
+++ display.py
@@ -99,9 +99,8 @@ def showWeather(disp,weather):
     print(weather['temp'])
     clearDisplay(disp)
     font=ImageFont.truetype('fonts/Arial.ttf',40)
-    drawRotatedText(disp.buffer,str(weather['temp'])+"°C",(64,80),270,font,fill=(255,255,255))
+    drawRotatedText(disp.buffer,str(weather['temp'])+"°C",(32,80),270,font,fill=(255,255,255))
     disp.display()
-
 
 def main():
     disp = createDisplay()
